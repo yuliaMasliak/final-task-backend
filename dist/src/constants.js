0263aa2c19ef6b7f52c24682de5aaf201c6031ac
Yulia Masliak <1985kozak@gmail.com>
2023-09-11 13:46:40 +0300

    feat: remove dist

--- dist/src/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SECRET_KEY = exports.PORT = void 0;
-exports.PORT = 3000;
-exports.SECRET_KEY = 'myVerySecretKey-04.05.2022';
